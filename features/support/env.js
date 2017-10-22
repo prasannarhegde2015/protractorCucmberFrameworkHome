1aa64d0576d075a0c4b17f9cbebca50005eab2cf
prasannarhegde2015 <prasannarhegde@gmail.com>
2017-10-22 14:31:21 +0530

    Update env.js

--- features/support/env.js
+++ features/support/env.js
@@ -3,3 +3,8 @@ let chaiAsPromised = require('chai-as-promised');
 
 chai.use(chaiAsPromised);
 global.expect = chai.expect;
+var configure = function () {
+    this.setDefaultTimeout(60 * 1000);
+};
+
+module.exports = configure;
